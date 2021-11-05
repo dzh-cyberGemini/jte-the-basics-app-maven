986e68677782d9c5a53cd14fd79593de7bfb6fee
Dzhaner Ahmed <77504842+dzh-cyberGemini@users.noreply.github.com>
2021-11-05 17:48:33 +0200

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -2,13 +2,13 @@ alow_scm_jenkinsfile = false
 
 libraries{
     maven{
-      message = "From MAVEN App repository main branch date 01.11.21!"
+      message = "From MAVEN App repository main branch date 05.11.21!"
     }
     docker{
       location = "DOCKER variable from Maven App repository!"     
     }
     gradle{
         message = "HELLO from GRADLE-app repository! This is message variable."
-        number = 5
+        number = 23
     }
 }
